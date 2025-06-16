a1f89a39b58e61e9ccd3d00d5d47598492f704c8
Raghavanoffl <140875182+Raghavanoffl@users.noreply.github.com>
2025-06-17 00:34:20 +0530

    .

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,9 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Raghavan</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Raghavann Self-taught MERN stack developer with a strong foundation in Artificial Intelligence and Data Science. I’m passionate about building intelligent, scalable applications that merge data-driven insights with real-world usability. My journey blends full-stack development skills with a deep curiosity for machine learning, model explainability, and innovative problem-solving. Always eager to learn and adapt, I focus on writing clean, efficient code and creating solutions that truly make an impact.<span>"
+
+
       ],
       typeSpeed: 7,
       contentType: 'html'
